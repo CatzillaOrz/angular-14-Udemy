1631d9d5e664ed4f8e2c3a264c50006100aa7edc
CatzillaOrz <catzillaorz@outlook.com>
2022-06-13 17:27:00 +0800

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1> <p-button label="Submit"></p-button>
+  template: `<h1>Hello {{name}}!</h1> 
   `,
   styles: [`h1 { font-family: Lato; }`],
 })
