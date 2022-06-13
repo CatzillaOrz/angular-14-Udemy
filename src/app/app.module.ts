f3815d309bb3af1cec20f65c563fdb557250e233
CatzillaOrz <catzillaorz@outlook.com>
2022-06-13 16:58:10 +0800

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AlertComponent } from './shared/alert/alert.component';
+import { ButtonModule } from 'primeng/button';
 
 const CUSTOM_COMPONENT = [AlertComponent];
+const PRIMENG_MODULE = [ButtonModule];
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ...PRIMENG_MODULE],
   declarations: [AppComponent, HelloComponent, ...CUSTOM_COMPONENT],
   bootstrap: [AppComponent],
 })
