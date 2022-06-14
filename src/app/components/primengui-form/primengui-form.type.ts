1de4db5880b86052176031ce1868c6baa5d4407e
CatzillaOrz <catzillaorz@outlook.com>
2022-06-14 16:52:15 +0800

    1 file modified, 3 added

--- src/app/components/primengui-form/primengui-form.type.ts
+++ src/app/components/primengui-form/primengui-form.type.ts
@@ -61,3 +61,18 @@ export type PrimenguiFormControlType =
   | 'tableInput';
   export type PrimenguiFormTheme = 'light' | 'dark';
   export type PrimenguiFormGroupType = 'accordion' | 'tabs';
+
+
+
+export const PrimenguiCalendarLang = {
+  firstDayOfWeek: 0,
+  dayNames: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
+  dayNamesShort: ['日', '一', '二', '三', '四', '五', '六'],
+  dayNamesMin: ['日', '一', '二', '三', '四', '五', '六'],
+  monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+  monthNamesShort: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+  today: '今天',
+  clear: '清除',
+  dateFormat: 'yy-mm-dd',
+  weekHeader: '周'
+};
---
+++ src/app/components/primengui-form/primengui-form/primengui-form-control/primengui-form-control.component.ts
@@ -0,0 +1,92 @@
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewEncapsulation,
+} from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import {
+  PrimenguiCalendarLang,
+  PrimenguiFormControl,
+} from '../../primengui-form.type';
+
+@Component({
+  selector: 'app-primengui-form-control',
+  templateUrl: './primengui-form-control.component.html',
+  styleUrls: ['./primengui-form-control.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class PrimenguiFormControlComponent implements OnInit, OnChanges {
+  @Input() formGroup: FormGroup;
+
+  @Input() control: PrimenguiFormControl = {};
+
+  @Input() labelWidth = '';
+
+  classMap: { [key: string]: boolean } = {};
+
+  calendarCN = PrimenguiCalendarLang;
+  public srcValue = '';
+
+  uploadedFiles: any[] = [];
+  PreviewWidth = 104;
+  file: File;
+
+  ngOnInit(): void {
+    this.setControl();
+    this.setClassMap();
+  }
+  ngOnChanges(changes: SimpleChanges): void {}
+
+  constructor(private messageService: MessageService) {}
+  getImgSrc(key) {
+    if (this.formGroup.controls && this.formGroup.controls[key]) {
+      return this.formGroup.controls[key].value || '';
+    } else {
+      return '';
+    }
+  }
+
+  blurFn() {}
+  blurNumberFn(key) {
+    let formValue = this.formGroup.getRawValue()[key];
+    let p = {};
+    p[key] = formValue;
+    this.formGroup.patchValue(p);
+  }
+  blurFdFn(key) {
+    let formValue = this.formGroup.getRawValue()[key];
+    let p = {};
+    p[key] = formValue;
+    if (formValue) {
+      let value = String(formValue).split('.');
+      if (value.length === 2) {
+        if (!value[0]) {
+          value[0] = '0';
+        }
+        if (!value[1]) {
+          value[0] = '0';
+        }
+        p[key] = Number(value.join('.'));
+      }
+    }
+    this.formGroup.patchValue(p);
+  }
+  onClear(key) {
+    this.formGroup.controls[key].setValue('');
+  }
+  onSelect(event, key) {
+    let file = event.files[0];
+    this.file = event.files[0];
+  }
+  setControl() {}
+
+  setClassMap() {
+    this.classMap = {
+      [`primengui-form-control-${this.control.type.toLocaleLowerCase()}`]: true,
+    };
+  }
+}
