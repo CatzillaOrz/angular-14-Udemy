73255c92d9903e2395fb439212b5330fdc8ffbb5
CatzillaOrz <catzillaorz@outlook.com>
2022-06-15 11:37:52 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,35 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  optFilesPropFormData = [
+    {
+      key: 'additional-files',
+      label: '附加文件',
+      layout: '12',
+      type: 'inputText',
+      required: false,
+      value: 'taz.add.xml',
+    },
+    {
+      key: 'net-file',
+      label: '路网文件',
+      layout: '12',
+      type: 'inputText',
+      required: false,
+      value: 'sumo.net.xml',
+    },
+    {
+      key: 'route-files',
+      label: '路由文件',
+      layout: '12',
+      type: 'inputText',
+      required: false,
+      value: '100_car.rou.xml',
+    },
+  ];
 }
