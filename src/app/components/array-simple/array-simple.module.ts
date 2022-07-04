24e8001891c2a07e00361529d3c269091a81387d
CatzillaOrz <catzillaorz@outlook.com>
2022-07-04 16:09:28 +0800

    2 files modified

--- src/app/components/array-simple/array-simple.module.ts
+++ src/app/components/array-simple/array-simple.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule,  } from '@angular/common';
 import { ArraySimpleComponent } from './array-simple.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
