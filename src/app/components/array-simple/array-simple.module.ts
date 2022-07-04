7a66b4a2d50502ed716f3be9f046bd6e2245e464
CatzillaOrz <catzillaorz@outlook.com>
2022-07-04 15:46:09 +0800

    1 file modified

--- src/app/components/array-simple/array-simple.module.ts
+++ src/app/components/array-simple/array-simple.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArraySimpleComponent } from './array-simple.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 
 const CUSTOM_COMPONENT = [ArraySimpleComponent];
 const PRIMENG_MODULES = [InputTextModule];
